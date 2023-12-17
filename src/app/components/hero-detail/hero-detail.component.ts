c0f1d352c96aaa70a6e8ce1adfee0d6b7a5a6841
akhapangee <yorkmacbook052@Yorks-MBP.lan>
2023-12-17 17:39:32 -0600

    getHero() fixing error

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -34,7 +34,7 @@ export class HeroDetailComponent implements OnInit {
    */
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    console.log(id)
+    if (!id) return;
     this.heroService.getHero(id).subscribe(hh => this.hero = hh);
 
   }
